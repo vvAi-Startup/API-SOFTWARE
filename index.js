1dcd6fc064b8d16139967c8e498f6e2c00350fd8
Leonardo Wicher <leonardo.wlferreira@gmail.com>
2024-10-24 22:16:09 -0300

    index

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const authRoutes = require('./routes/authRoutes');  // Importar as rotas
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
 
-const app = express();
-
-// Middleware para processar JSON
-app.use(express.json());
-
-// Conectar ao MongoDB (usando 127.0.0.1 para evitar problemas com ::1/IPv6)
-mongoose.connect('mongodb://127.0.0.1:27017/calmwave')
-  .then(() => console.log('MongoDB conectado com sucesso.'))
-  .catch((error) => console.log('Erro ao conectar ao MongoDB:', error));
-
+// Importar as rotas
+//
 // Rotas de autenticação
 app.use('/api/auth', authRoutes);
+
+// Middleware para processar JSON
+app.use(express.json())
+
+// Conectar ao MongoDB (usando 127.0.0.1 para evitar problemas com ::1/IPv6)
+// mongoose.connect('mongodb://127.0.0.1:27017/calmwave')
+//   .then(() => console.log('MongoDB conectado com sucesso.'))
+//   .catch((error) => console.log('Erro ao conectar ao MongoDB:', error));
+
 
 // Iniciar o servidor
 const PORT = process.env.PORT || 5000;
